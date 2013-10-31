8b3bf3c9f393db1c9b3c922a181cfadfc4a91528
Jon Galloway <jongalloway@gmail.com>
2013-10-30 17:40:40 -0700

    Added some danger

---
+++ danger/seriously/WebApplication24/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication24.Startup))]
+namespace WebApplication24
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
